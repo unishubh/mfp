2f757f845b8460c44892a6aa7fd0a481195eda6a
Shubh Shukla <unishubh1@gmail.com>
2020-02-11 23:44:43 +0530

    refs from open thread removed

--- services/postbackHandler/postbackHandler.js
+++ services/postbackHandler/postbackHandler.js
@@ -16,12 +16,8 @@ module.exports = class PostbackHandler {
         let postback = this.webhookEvent.postback;
         // Check for the special Get Starded with referral
         let payload;
-        if ( postback.referral && postback.referral.type == "OPEN_THREAD" ) {
-            payload = postback.referral.ref;
-        } else {
             // Get the payload of the postback
             payload = postback.payload;
-        }
         let handler = new QuickRepliesHandler(this.user, this.webhookEvent);
         return handler.handlePayload(payload.toUpperCase());
     }
