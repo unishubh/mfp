87159095915f8a4f5a257d5ee67baf48525bac5a
rini pandey <rinipandey1910@gmail.com>
2020-05-02 10:34:39 +0530

    modified queries

--- constants/db.sql
+++ constants/db.sql
@@ -64,6 +64,7 @@ PRIMARY KEY ( id )
 create TABLE Client (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ( id )
 );
